23cca1ea20e72a979ee95e1733a6f6dd1d44c373
Melissa Mohan <melissam@nulogy.com>
2019-10-15 15:25:44 -0400

    adding car speed

--- car.rb
+++ car.rb
@@ -4,33 +4,34 @@ class Car
   end
 
   def accelerate
-    @speed = @speed + 5
+    @speed += 5
     puts "Current speed is #{@speed}"
   end
 
   def brake
-   @speed = @speed - 10
-   puts "Current speed is #{@speed}"
+    @speed -= 10
+    if @speed < 0
+      puts "The car is stopped"
+      @speed = 0
+    else
+      puts "Current speed is #{@speed}"
+    end
   end
 end
 
-current = 0
-current_speed = Car.new(current)
+inital_speed = 0
+car = Car.new(inital_speed)
 puts "Welcome to the Car simulator!"
-puts "What would you like to do next?"
-action = gets.chomp
+action = nil
 
 while action != "exit" do
-  if action == "accelerate"
-    current_speed.accelerate
-    puts "What would you like to do next?"
-    action = gets.chomp
-  elsif action == "brake"
-    current_speed.brake
-    puts "What would you like to do next?"
-    action = gets.chomp
+  puts "What would you like to do next?"
+  action = gets.chomp
+  case action
+  when "accelerate"
+    car.accelerate
+  when "brake"
+    car.brake
   else puts "Invalid entry, please try action"
-     puts "What would you like to do next?"
-     action = gets.chomp
   end
 end
