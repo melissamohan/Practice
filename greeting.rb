7e031cf608576f015b4d6b81c497b6ccab1c9d5a
Melissa Mohan <melissam@nulogy.com>
2019-10-29 08:01:22 -0400

    adding greeting kata

--- greeting.rb
+++ greeting.rb
@@ -2,8 +2,42 @@ class Greeting
   def greet(name = nil)
     if name.nil?
       "Hello, my friend."
+    elsif name == name.upcase
+      "HELLO #{name}!"
     else
-      "Hello, Bob."
+      "Hello, #{name}."
     end
   end
+
+  def greet2(name1, name2)
+    "Hello, #{name1} and #{name2}."
+  end
+
+  def greet_multiple(*name)
+    last = name.pop()
+    "Hello, #{name.join(', ')}, and #{last}."
+  end
+
+  def greet_multiple_shout(*name)
+    last = name.pop()
+    y = name.select { |x| x != x.upcase }
+    s = name.select { |r| r == r.upcase }
+    "Hello, #{y.join(', ')} and #{last}. AND HELLO #{s.join('')}!"
+  end
+
+  def greet_split_commas(*name)
+    y = name.join(',')
+    p = y.split(",")
+    last = p.pop()
+    "Hello, #{p.join(', ')}, and#{last}."
+  end
+
+  def greet_escape_intentional_commas(*name)
+    p = name.join(',')
+    y = p.gsub("\"", "")
+    x = y.split(",")
+    last = x.pop()
+    "Hello, #{x.join(' and ')},#{last}."
+  end
+
 end
--- spec/greeting_spec.rb
+++ spec/greeting_spec.rb
@@ -10,10 +10,60 @@ RSpec.describe Greeting do
   end
 
   it "greets with a name" do
-    greeter =  Greeting.new
+    greeter = Greeting.new
 
     result = greeter.greet("Bob")
 
     expect(result).to eq("Hello, Bob.")
   end
+
+  it "greets with a shout" do
+    greeter = Greeting.new
+
+    result = greeter.greet("JERRY")
+
+    expect(result).to eq("HELLO JERRY!")
+  end
+
+  it "greets with two names" do
+    greeter = Greeting.new
+
+    result = greeter.greet2("Jill", "Jane")
+
+    expect(result).to eq("Hello, Jill and Jane.")
+  end
+
+  it "greets with multiple names" do
+    greeter = Greeting.new
+
+    result = greeter.greet_multiple("Amy", "Brian", "Charlotte")
+
+    expect(result).to eq("Hello, Amy, Brian, and Charlotte.")
+  end
+
+  it "greets with multiple names including shouting" do
+    greeter = Greeting.new
+
+    result = greeter.greet_multiple_shout("Amy", "BRIAN", "Charlotte")
+
+    expect(result).to eq("Hello, Amy and Charlotte. AND HELLO BRIAN!")
+  end
+
+  it "greets with multiple names, split using commas" do
+    greeter = Greeting.new
+
+    result = greeter.greet_split_commas("Bob", "Charlie, Dianne")
+
+    expect(result).to eq("Hello, Bob, Charlie, and Dianne.")
+  end
+
+  it "greets with multiple names, escapes intentional commas" do
+    greeter = Greeting.new
+
+    result = greeter.greet_escape_intentional_commas("Bob", "\"Charlie, Dianne\"")
+
+    expect(result).to eq("Hello, Bob and Charlie, Dianne.")
+  end
+
+
 end
